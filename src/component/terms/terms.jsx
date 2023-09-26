5169521ea286331f83fb07a9b2d0587ef9642c20
InchaeNa <dlsco1230@gmail.com>
2023-09-27 06:00:21 +0900

    "modal"

--- src/component/terms/terms.jsx
+++ src/component/terms/terms.jsx
@@ -3,20 +3,19 @@ import "./terms.css";
 
 const Terms = ({ closeModal }) => {
   return (
-    <div className="terms-index">
-      <div className="overlap-wrapper">
-        <div className="overlap">
-          <div className="modal-window" />
-          <div className="view-2">
-            <div className="text-wrapper-8">사진 업로드 약관</div>
-            <p className="AI-AI-AI-AI">
+    <div id="modalWrap">
+      <div id="modalContent">
+        <div id="modalBody" className="modal-body">
+        <div className="text-wrapper-8" style={{ textAlign: "center", fontWeight: "bold" }}>
+            사진 업로드 약관
+          </div><br /><br />
               러브러브샷은 커플 AI 이미지 서비스 제공을 위해 개인정보를 수집 및
               이용합니다.
-              <br />
+              <br /><br />
               수집하는 개인 정보 항목 : 이용자 사진, 성별
-              <br />
+              <br /><br />
               수집 및 이용 목적 : 커플 AI 이미지 생성
-              <br />
+              <br /><br />
               보유 및 이용기간
               <br />
               사용자가 제공한 사진, 성별 : 이미지 생성시까지
@@ -24,8 +23,7 @@ const Terms = ({ closeModal }) => {
               제작된 커플 이미지 : 사용자 삭제 요청시까지
               <br />
               <br />
-              사용자의 동의는 커플 AI 이미지 생성 서비스 종료시까지 <br />
-              유지되며,
+              사용자의 동의는 커플 AI 이미지 생성 서비스 종료시까지 유지되며,
               <br />
               개인정보의 수집 및 이용에 대한 동의를 거부할 수 있습니다.
               <br />
@@ -36,17 +34,11 @@ const Terms = ({ closeModal }) => {
               사용자가 AI생성물로 인해 제3자로부터 권리 침해 주장을 당하는 경우
               회사는 <br />
               어떠한 책임도 부담하지 않습니다.
-            </p>
-          </div>
-          <div className="x" onClick={closeModal}>
-            <div className="overlap-2">
-              <div className="rectangle-4" />
-              <div className="rectangle-5" />
-            </div>
-          </div>
-        </div>
+              </div>
+        <div className="closeBtn" onClick={closeModal}></div>
       </div>
     </div>
   );
 };
+
 export default Terms;
