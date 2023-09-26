62662efc8c6468493e6cb148d524e19d3600d9d6
UncleSubak <kko6369@naver.com>
2023-09-26 15:52:03 +0900

    feat: 카카오 로그아웃 구현

---
+++ src/oauth2/OAuth2LogOutRedirect.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+const OAuth2LogOutRedirect = () => {
+  //   const location = useNavigate();
+  const jwt = localStorage.getItem("jwt");
+  if (jwt) {
+    localStorage.removeItem("jwt");
+    // location("/");
+    window.location.replace("/");
+  }
+};
+export default OAuth2LogOutRedirect;
